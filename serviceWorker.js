5164cf0b28990eefa7429837bd25ff88fa1d957f
Bob_work <musgravehill@gmail.com>
2016-06-06 16:28:27 +0400

    html

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,59 +1,65 @@
 'use strict';
 
+var customUrl = '/';
+
 console.log('Started', self);
 
 self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  console.log('ServiceWorker Installed', event);
+    self.skipWaiting();
+    console.log('ServiceWorker Installed', event);
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('ServiceWorker Activated', event);
+    console.log('ServiceWorker Activated', event);
 });
 
 self.addEventListener('push', function(event) {
-  console.log('ServiceWorker Push message', event);
+    console.log('ServiceWorker Push message', event);
 
-      event.waitUntil(  
-      fetch('/gcm-notify.json').then(function(response) { 
-            return response.json().then(function(data) { 
+    event.waitUntil(
+            fetch('/gcm-notify.json').then(function(response) {
+        return response.json().then(function(data) {
             console.log(JSON.stringify(data));
-                var title = data.title;  
-                var body = data.body;  
-				var icon = data.icon;
-                var url=data.url;     
+            var title = data.title;
+            var body = data.body;
+            var icon = data.icon;
+            customUrl = data.url;
 
-                return self.registration.showNotification(title, {  
-                  body: body,  
-                  icon: icon,  
-                  tag: 'notificationTag'  
-                }); 
+            return self.registration.showNotification(title, {
+                body: body,
+                icon: icon,
+                tag: 'notificationTag'
+            });
 
-            });  
-      }).catch(function(err) {  
-          console.error('Unable to retrieve data', err);  
-        })  
-      );   
+        });
+    }). catch (function(err) {
+        console.error('Unable to retrieve data', err);
+    })
+            );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
-  // See: http://crbug.com/463146
-  event.notification.close();
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn’t close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
 
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
+    clients.openWindow(customUrl);
+    
+    /*
+     // This looks to see if the current is already open and
+     // focuses if it is
+     event.waitUntil(clients.matchAll({
+     type: "window"
+     }).then(function(clientList) {
+     for (var i = 0; i < clientList.length; i++) {
+     var client = clientList[i];
+     if (client.url == customUrl && 'focus' in client)
+     return client.focus();
+     }
+     if (clients.openWindow)
+     return clients.openWindow(customUrl);
+     }));
+     */
 
 });
