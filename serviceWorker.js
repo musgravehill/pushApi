efe357533018491106b6287f321e8bcc8a9ec796
Bob_work <musgravehill@gmail.com>
2016-06-06 16:11:34 +0400

    click

--- serviceWorker.js
+++ serviceWorker.js
@@ -35,3 +35,25 @@ self.addEventListener('push', function(event) {
         })  
       );   
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+
+});
