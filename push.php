8611646c57437f8ecea80a32a2fb37d154e734f3
Bob_work <musgravehill@gmail.com>
2016-06-06 16:05:02 +0400

    gcm

--- push.php
+++ push.php
@@ -5,7 +5,7 @@ $data = array();
 $handle = fopen("endpoints.txt", "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
-        $data['registration_ids'][] = str_replace('https://android.googleapis.com/gcm/send/', '', $line);
+        $data['registration_ids'][] = str_replace(array('https://android.googleapis.com/gcm/send/', PHP_EOL), '', $line);
     }
     fclose($handle);
 } else {
@@ -14,6 +14,8 @@ if ($handle) {
 
 $url = "https://android.googleapis.com/gcm/send";
 $content = json_encode($data);
+
+echo $content.'<br><br>';
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_HEADER, false); // don't return headers
@@ -29,7 +31,7 @@ $response = curl_exec($curl);
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 if ($status != 201) {
-    die("Error: call to URL $url failed with status $status, response $response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    die("Error: call to URL $url failed with status $status, response $response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl)).'<br><br>';
 }
 
 curl_close($curl);
