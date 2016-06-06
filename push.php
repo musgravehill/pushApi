5f9c80bf559a33fe6a64daaf68d94300212a77bc
Bob_work <musgravehill@gmail.com>
2016-06-06 15:52:11 +0400

    push

--- push.php
+++ push.php
@@ -1,11 +1,37 @@
-<?php 
+<?php
+
+$data = array();
 
 $handle = fopen("endpoints.txt", "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
-        $endpoint = str_replace('https://android.googleapis.com/gcm/send/');
+        $data['registration_ids'][] = str_replace('https://android.googleapis.com/gcm/send/', '', $line);
     }
     fclose($handle);
 } else {
-    // error opening the file.
-} 
+    die();
+}
+
+$url = "https://android.googleapis.com/gcm/send";
+$content = json_encode($data);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false); // don't return headers
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json", "Authorization: key=AIzaSyBxmzk9L0qaQIWSCSr_BtpzxqarCdYoPfU"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+curl_setopt($curl, CURLOPT_ENCODING, "utf-8");
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if ($status != 201) {
+    die("Error: call to URL $url failed with status $status, response $response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+}
+
+curl_close($curl);
+
+print_r($response);
